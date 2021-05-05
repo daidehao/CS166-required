b16a555fa2708037229e7a72161de96f62336891
Dehao Dai <ddai009@wch133-36.cs.ucr.edu>
2021-05-05 09:55:19 -0700

    add solution

---
+++ lab5/queries.sql
@@ -0,0 +1,20 @@
+SELECT parts.pid 
+FROM parts, catalog 
+WHERE catalog.cost<10 AND catalog.pid=parts.pid;
+
+SELECT parts.pname
+FROM parts, catalog
+WHERE catalog.cost<10 AND catalog.pid=parts.pid;
+
+SELECT suppliers.address
+FROM suppliers, parts, catalog
+WHERE parts.pname like '%Fire Hydrant Cap%'AND suppliers.sid=catalog.sid AND catalog.pid=parts.pid;
+
+SELECT suppliers.sname
+FROM suppliers, parts, catalog
+WHERE (parts.color='green' OR parts.color='Green') AND suppliers.sid=catalog.sid AND catalog.pid=parts.pid;
+
+SELECT suppliers.sname, parts.pname
+FROM suppliers, parts, catalog
+WHERE suppliers.sid=catalog.sid AND catalog.pid=parts.pid;
+
