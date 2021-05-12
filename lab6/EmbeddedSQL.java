2d19bbc0c6a24931de54058de2bfcb62171af2df
daidehao <56137418+daidehao@users.noreply.github.com>
2021-05-12 11:46:01 -0700

    Update EmbeddedSQL.java

--- lab6/EmbeddedSQL.java
+++ lab6/EmbeddedSQL.java
@@ -265,20 +265,59 @@ public class EmbeddedSQL {
 
    public static void Query2(EmbeddedSQL esql){
       // Your code goes here.
-      // ...
-      // ...
+      try{
+        String query = "SELECT A.sname AS Suppliers_Name, A.Parts_Count AS Parts_Count_3MORE "+
+           "FROM (SELECT suppliers.sname, COUNT(*) AS parts_count FROM catalog, suppliers WHERE suppliers.sid=catalog.sid GROUP BY suppliers.sid) AS A "+
+           "WHERE A.parts_count>=3";
+
+           int rowCount = esql.executeQuery(query);
+           System.out.println ("total row(s): " + rowCount);
+      }catch(Exception e){
+         System.err.println (e.getMessage());
+      }
    }//end Query2
 
    public static void Query3(EmbeddedSQL esql){
       // Your code goes here.
-      // ...
-      // ...
+      try{
+        String query = "SELECT suppliers.sname AS Suppliers_Name, COUNT(*) AS Parts_Count "+
+           "FROM catalog, suppliers, parts "+
+           "WHERE suppliers.sid=catalog.sid AND catalog.pid=parts.pid AND parts.color='Green' "+
+           "GROUP BY suppliers.sid "+
+    	   "EXCEPT "+
+	   "SELECT suppliers.sname AS Suppliers_Name, COUNT(*) AS Parts_Count "+
+  	   "FROM catalog, suppliers, parts "+
+           "WHERE suppliers.sid=catalog.sid AND catalog.pid=parts.pid AND parts.color!='Green '+
+           "GROUP BY suppliers.sid";
+
+         int rowCount = esql.executeQuery(query);
+         System.out.println ("total row(s): " + rowCount);
+      }catch(Exception e){
+         System.err.println (e.getMessage());
+      }
    }//end Query3
 
    public static void Query4(EmbeddedSQL esql){
       // Your code goes here.
-      // ...
-      // ...
+      try{
+        String query = "SELECT suppliers.sname AS Supplier_Name, MAX(catalog.cost) AS MAX_PRICE "+
+           "FROM catalog, suppliers "+
+           "WHERE suppliers.sid=catalog.sid AND suppliers.sid IN ( "+
+	   "SELECT suppliers.sid "+
+	   "FROM catalog, suppliers, parts "+
+	   "WHERE suppliers.sid=catalog.sid AND catalog.pid=parts.pid AND parts.color='Green' "+
+	   "INTERSECT "+
+	   "SELECT suppliers.sid "+
+	   "FROM catalog, suppliers, parts "+
+	   "WHERE suppliers.sid=catalog.sid AND catalog.pid=parts.pid AND parts.color='Red' "+
+	   ") "+
+	   "GROUP BY suppliers.sname";
+
+         int rowCount = esql.executeQuery(query);
+         System.out.println ("total row(s): " + rowCount);
+      }catch(Exception e){
+         System.err.println (e.getMessage());
+      }
    }//end Query4
 
    public static void Query5(EmbeddedSQL esql){
