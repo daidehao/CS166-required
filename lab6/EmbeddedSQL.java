c0f5efdaa52927735fc241d86ef4701a842a4cb1
daidehao <56137418+daidehao@users.noreply.github.com>
2021-05-12 11:56:13 -0700

    Update EmbeddedSQL.java

--- lab6/EmbeddedSQL.java
+++ lab6/EmbeddedSQL.java
@@ -323,7 +323,7 @@ public class EmbeddedSQL {
    public static void Query5(EmbeddedSQL esql){
       // Your code goes here.
       try{
-         String query = "SELECT parts.pname FROM Catalog, parts WHERE Catalog.pid=parts.pid Catalog.cost < ";
+         String query = "SELECT parts.pname FROM catalog, parts WHERE catalog.pid=parts.pid AND catalog.cost < ";
          System.out.print("\tEnter cost: $");
          String input = in.readLine();
          query += input;
