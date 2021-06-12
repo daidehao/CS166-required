30b095a41d48580e49ba1ea59d496b0bf7c9681c
daidehao <56137418+daidehao@users.noreply.github.com>
2021-06-11 21:24:11 -0700

    Update create_index.sql

--- project/sql/create_index.sql
+++ project/sql/create_index.sql
@@ -1,2 +1,6 @@
 CREATE INDEX Patient_index on Patient USING BTREE(patient_ID);
 CREATE INDEX Hospital_index on Hospital USING BTREE(hospital_ID);
+CREATE INDEX Department_index on Department USING BTREE(dept_ID);
+CREATE INDEX Staff_index on Staff USING BTREE(staff_ID);
+CREATE INDEX Doctor_index on Doctor USING BTREE(doctor_ID);
+CREATE INDEX Appointment_index on Appointment USING BTREE(appnt_ID);
